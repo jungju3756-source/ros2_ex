381260ac959a73200c5e5c4e5ab7aa64f908cf08
jungju3756-source <jungju3756@gmail.com>
2025-11-04 11:02:46 +0900

    Add files via upload

---
+++ pub1_1/src/pub1_1.cpp
@@ -0,0 +1,34 @@
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include <memory>
+#include <chrono>
+
+int main(int argc, char * argv[])
+{
+  rclcpp::init(argc, argv);
+
+  auto node = std::make_shared<rclcpp::Node>("node_pub1_1");
+
+  auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
+
+  auto mypub = node->create_publisher<std_msgs::msg::Int32>("topic_pub1_1", qos_profile);
+
+  std_msgs::msg::Int32 message;
+  message.data = 0;
+
+  rclcpp::WallRate loop_rate(1.0);
+
+  while (rclcpp::ok())
+  {
+
+    RCLCPP_INFO(node->get_logger(), "Publish: %d", message.data);
+    
+    mypub->publish(message);
+    message.data++; 
+    
+    loop_rate.sleep();
+  }
+
+  rclcpp::shutdown();
+  return 0;
+}

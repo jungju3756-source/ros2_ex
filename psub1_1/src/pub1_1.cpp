18c22a7ab28e8775ca364e554822f6b643c6298e
jungju3756-source <jungju3756@gmail.com>
2025-11-18 09:45:36 +0900

    Update pub1_1.cpp

--- psub1_1/src/pub1_1.cpp
+++ psub1_1/src/pub1_1.cpp
@@ -1,24 +1,35 @@
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
-#include "rclcpp/time_source.hpp"
 #include <memory>
 #include <chrono>
-int main(int argc, char * argv[])
+#include <functional>
+
+using namespace std::chrono_literals;
+
+void callback(rclcpp::Node::SharedPtr node,rclcpp::Publisher<std_msgs::msg::String>::SharedPtr mypub)
 {
-rclcpp::init(argc, argv);
+    auto message = std_msgs::msg::String();
+    message.data = "Hello world!";  
+
+    RCLCPP_INFO(node->get_logger(), "Publish: %s", message.data.c_str());
+    mypub->publish(message);
+}
+
+int main(int argc, char* argv[])
+{
+    rclcpp::init(argc, argv);
     auto node = std::make_shared<rclcpp::Node>("node_pub1");
     auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10));
-    auto mypub = node->create_publisher<std_msgs::msg::String>("topic_pub1", qos_profile );
-    std_msgs::msg::String message;
-    message.data = "Hello world!";
-    rclcpp::WallRate loop_rate(10.0);
-    while(rclcpp::ok())
-{
-    RCLCPP_INFO(node->get_logger(), "Publish: %s",
-    message.data.c_str());
-    mypub->publish(message);
-    loop_rate.sleep();
-}
+
+    auto pub = node->create_publisher<std_msgs::msg::String>(
+        "topic_pub1", qos_profile);
+
+    std::function<void()> fn = std::bind(callback, node, pub);
+
+    // 10Hz timer
+    auto timer = node->create_wall_timer(100ms, fn);
+
+    rclcpp::spin(node);
     rclcpp::shutdown();
     return 0;
 }

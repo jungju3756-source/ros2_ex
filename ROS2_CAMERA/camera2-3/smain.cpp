1d4fd1d4345032d59a4358065bfd4c161489b002
jungju3756-source <jungju3756@gmail.com>
2025-11-18 21:55:41 +0900

    Create smain.cpp

---
+++ ROS2_CAMERA/camera2-3/smain.cpp
@@ -0,0 +1,9 @@
+#include "sub.hpp"
+
+int main(int argc, char* argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<CameraSubscriber>();
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}

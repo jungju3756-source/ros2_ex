48c69334ec691f2220987110a57ebfa73bfa5144
jungju3756-source <jungju3756@gmail.com>
2025-11-18 21:46:30 +0900

    Create sub.cpp

---
+++ ROS2_CAMERA/camera2-1/sub.cpp
@@ -0,0 +1,35 @@
+#include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/compressed_image.hpp"
+#include <opencv2/opencv.hpp>
+#include <memory>
+#include <iostream>
+
+void image_callback(const sensor_msgs::msg::CompressedImage::SharedPtr msg, const std::string& window_name) {
+    cv::Mat im = cv::imdecode(cv::Mat(msg->data), cv::IMREAD_COLOR);
+    if (!im.empty()) {
+        cv::imshow(window_name, im);
+        cv::waitKey(1);
+    }
+}
+
+int main(int argc, char * argv[]) {
+    rclcpp::init(argc, argv);
+    auto node = std::make_shared<rclcpp::Node>("camera2_sub");
+    auto qos_profile = rclcpp::QoS(rclcpp::KeepLast(10)).best_effort();
+
+    auto sub_raw = node->create_subscription<sensor_msgs::msg::CompressedImage>(
+        "image_raw", qos_profile,
+        [node](const sensor_msgs::msg::CompressedImage::SharedPtr msg) { image_callback(msg, "원본영상"); });
+
+    auto sub_gray = node->create_subscription<sensor_msgs::msg::CompressedImage>(
+        "image_gray", qos_profile,
+        [node](const sensor_msgs::msg::CompressedImage::SharedPtr msg) { image_callback(msg, "그레이영상"); });
+
+    auto sub_edge = node->create_subscription<sensor_msgs::msg::CompressedImage>(
+        "image_edge", qos_profile,
+        [node](const sensor_msgs::msg::CompressedImage::SharedPtr msg) { image_callback(msg, "에지영상"); });
+
+    rclcpp::spin(node);
+    rclcpp::shutdown();
+    return 0;
+}

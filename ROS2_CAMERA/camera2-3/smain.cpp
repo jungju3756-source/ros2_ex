36879dba42feac801d6ba1e837744b8555f8d24f
jungju3756-source <jungju3756@gmail.com>
2025-11-26 15:11:44 +0900

    Delete ROS2_CAMERA/camera2-3 directory

--- ROS2_CAMERA/camera2-3/smain.cpp
+++
@@ -1,9 +0,0 @@
-#include "sub.hpp"
-
-int main(int argc, char* argv[]) {
-    rclcpp::init(argc, argv);
-    auto node = std::make_shared<CameraSubscriber>();
-    rclcpp::spin(node);
-    rclcpp::shutdown();
-    return 0;
-}
